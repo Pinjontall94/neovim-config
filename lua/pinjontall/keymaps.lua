8c85949089a2334cf1b7ea50e5e9d3831ef3ba93
Sammi Johnson <sambjohnson94@protonmail.com>
2023-08-02 02:58:54 -0700

    added mrow single file layout, ignore plugin

--- lua/pinjontall/keymaps.lua
+++
@@ -1,9 +0,0 @@
---   --------------
---  -- Keymaps -- 
--- -------------
-
--- Set space bar as leader key
-vim.g.mapleader = " "
-
--- Map :Ex to <SPC>pv (i.e. "project view")
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
